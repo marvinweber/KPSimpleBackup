30448eb7aaf097d450f5087187044fe4d9d5e8bb
weberonede <marvin@weberone.de>
2019-03-20 20:53:37 +0100

    Design Tweaks and more settings
    Added new settings for file amount, wether to use db-name or file-name, etc.

--- KPSimpleBackup/SettingsForm.Designer.cs
+++ KPSimpleBackup/SettingsForm.Designer.cs
@@ -31,74 +31,72 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.labelSelectedBackupPath = new System.Windows.Forms.Label();
             this.buttonSelectFolder = new System.Windows.Forms.Button();
-            this.buttonSave = new System.Windows.Forms.Button();
+            this.numericNumberOfBackups = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             this.labelSettingsIntro = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.checkBoxUseDbName = new System.Windows.Forms.CheckBox();
+            this.tabControlSettings = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.panel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNumberOfBackups)).BeginInit();
+            this.tabControlSettings.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.tableLayoutPanel1);
-            this.panel1.Controls.Add(this.buttonSave);
             this.panel1.Controls.Add(this.labelSettingsIntro);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 140);
+            this.panel1.Size = new System.Drawing.Size(420, 120);
             this.panel1.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.81081F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.18919F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.07748F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.92252F));
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericNumberOfBackups, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 16);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 21);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.43478F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 69.56522F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(387, 92);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(414, 96);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 26);
+            this.label1.Size = new System.Drawing.Size(142, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Number of backups to keep:\r\n(not working yet)";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 44);
-            this.label2.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Folder for backup files:";
+            this.label1.Text = "Number of backups to keep:";
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.labelSelectedBackupPath);
             this.panel2.Controls.Add(this.buttonSelectFolder);
-            this.panel2.Location = new System.Drawing.Point(161, 32);
+            this.panel2.Location = new System.Drawing.Point(153, 31);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(222, 56);
+            this.panel2.Size = new System.Drawing.Size(257, 56);
             this.panel2.TabIndex = 4;
             // 
             // labelSelectedBackupPath
@@ -120,9 +118,37 @@
             this.buttonSelectFolder.UseVisualStyleBackColor = true;
             this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
             // 
+            // numericNumberOfBackups
+            // 
+            this.numericNumberOfBackups.Location = new System.Drawing.Point(153, 4);
+            this.numericNumberOfBackups.Name = "numericNumberOfBackups";
+            this.numericNumberOfBackups.Size = new System.Drawing.Size(120, 20);
+            this.numericNumberOfBackups.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 43);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 15, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folder for backup files:";
+            // 
+            // labelSettingsIntro
+            // 
+            this.labelSettingsIntro.AutoSize = true;
+            this.labelSettingsIntro.Location = new System.Drawing.Point(3, 5);
+            this.labelSettingsIntro.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.labelSettingsIntro.Name = "labelSettingsIntro";
+            this.labelSettingsIntro.Size = new System.Drawing.Size(390, 13);
+            this.labelSettingsIntro.TabIndex = 1;
+            this.labelSettingsIntro.Text = "Please specify following options (at least the path is needed for backups to work" +
+    "):";
+            // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(322, 114);
+            this.buttonSave.Location = new System.Drawing.Point(377, 177);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 2;
@@ -130,29 +156,56 @@
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
-            // labelSettingsIntro
+            // checkBoxUseDbName
             // 
-            this.labelSettingsIntro.AutoSize = true;
-            this.labelSettingsIntro.Location = new System.Drawing.Point(3, 0);
-            this.labelSettingsIntro.Name = "labelSettingsIntro";
-            this.labelSettingsIntro.Size = new System.Drawing.Size(390, 13);
-            this.labelSettingsIntro.TabIndex = 1;
-            this.labelSettingsIntro.Text = "Please specify following options (at least the path is needed for backups to work" +
-    "):";
+            this.checkBoxUseDbName.AutoSize = true;
+            this.checkBoxUseDbName.Location = new System.Drawing.Point(6, 6);
+            this.checkBoxUseDbName.Name = "checkBoxUseDbName";
+            this.checkBoxUseDbName.Size = new System.Drawing.Size(335, 30);
+            this.checkBoxUseDbName.TabIndex = 7;
+            this.checkBoxUseDbName.Text = "Use Database-Name (File -> Database-Settings -> Name) instead \r\nof File-Name (Def" +
+    "ault) for Backup-File-Names";
+            this.checkBoxUseDbName.UseVisualStyleBackColor = true;
             // 
-            // numericUpDown1
+            // tabControlSettings
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(161, 4);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 5;
+            this.tabControlSettings.Controls.Add(this.tabPage1);
+            this.tabControlSettings.Controls.Add(this.tabPage2);
+            this.tabControlSettings.Location = new System.Drawing.Point(12, 12);
+            this.tabControlSettings.Name = "tabControlSettings";
+            this.tabControlSettings.SelectedIndex = 0;
+            this.tabControlSettings.Size = new System.Drawing.Size(440, 159);
+            this.tabControlSettings.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(432, 133);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "General";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.checkBoxUseDbName);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(432, 133);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Advanced";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 168);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(464, 209);
+            this.Controls.Add(this.tabControlSettings);
+            this.Controls.Add(this.buttonSave);
             this.Name = "SettingsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Settings";
@@ -162,7 +215,11 @@
             this.tableLayoutPanel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericNumberOfBackups)).EndInit();
+            this.tabControlSettings.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -177,6 +234,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label labelSelectedBackupPath;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericNumberOfBackups;
+        private System.Windows.Forms.CheckBox checkBoxUseDbName;
+        private System.Windows.Forms.TabControl tabControlSettings;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
