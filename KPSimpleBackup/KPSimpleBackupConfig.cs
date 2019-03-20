dc7e9416c4fa7d337f2cd29692465d73427a0aa2
weberonede <marvin@weberone.de>
2019-03-20 20:50:53 +0100

    Add new settings options and remove unused option
    BackupName option removed, because either Database-Name or File-Name will be used.
    Added options for amount of files to keep and wether to use DB-Name or File-Name for Backup-File-Name.

--- KPSimpleBackup/KPSimpleBackupConfig.cs
+++ KPSimpleBackup/KPSimpleBackupConfig.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using KeePass.App.Configuration;
 
 namespace KPSimpleBackup
@@ -10,6 +6,11 @@ namespace KPSimpleBackup
     public class KPSimpleBackupConfig
     {
         private AceCustomConfig customConfig;
+
+        // default config values
+        private static readonly String DEFAULT_BACKUP_PATH = "";
+        private static readonly long DEFAULT_FILE_AMOUNT_TO_KEEP = 15;
+        private static readonly bool DEFAULT_USE_DATABASE_NAMES_FOR_BACKUP_FILES = false;
 
         public KPSimpleBackupConfig(AceCustomConfig customConfig)
         {
@@ -20,12 +21,7 @@ namespace KPSimpleBackup
         {
             get
             {
-                return this.customConfig.GetBool("KPSimpleBackupConfig_backupConfigured", false);
-            }
-
-            set
-            {
-                this.customConfig.SetBool("KPSimpleBackupConfig_backupConfigured", value);
+                return this.BackupPath != DEFAULT_BACKUP_PATH;
             }
         }
 
@@ -33,7 +29,7 @@ namespace KPSimpleBackup
         {
             get
             {
-                return this.customConfig.GetString("KPSimpleBackupConfig_backupPath");
+                return this.customConfig.GetString("KPSimpleBackupConfig_backupPath", DEFAULT_BACKUP_PATH);
             }
 
             set
@@ -42,16 +38,29 @@ namespace KPSimpleBackup
             }
         }
 
-        public String BackupName
+        public long FileAmountToKeep
         {
             get
             {
-                return this.customConfig.GetString("KPSimpleBackupConfig_backupName", "");
+                return this.customConfig.GetLong("KPSimpleBackupConfig_fileAmountToKeep", DEFAULT_FILE_AMOUNT_TO_KEEP);
             }
 
             set
             {
-                this.customConfig.SetString("KPSimpleBackupConfig_backupName", value);
+                this.customConfig.SetLong("KPSimpleBackupConfig_fileAmountToKeep", value);
+            }
+        }
+
+        public bool UseDatabaseNameForBackupFiles
+        {
+            get
+            {
+                return this.customConfig.GetBool("KPSimpleBackupConfig_useDatabaseNameForBackupFiles", DEFAULT_USE_DATABASE_NAMES_FOR_BACKUP_FILES);
+            }
+
+            set
+            {
+                this.customConfig.SetBool("KPSimpleBackupConfig_useDatabaseNameForBackupFiles", value);
             }
         }
     }
