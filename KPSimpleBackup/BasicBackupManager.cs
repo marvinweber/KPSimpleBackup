7bebfc2cd29424853ab3553373af9274592804f1
marvinweber <marvin@marvinweber.net>
2020-05-15 18:07:39 +0200

    Implement CleanupManager
    Both, BasicBackups and LongTermBackups
    now use the new CleanupManager to cleanup
    after backup.
    
    See #20

--- KPSimpleBackup/BasicBackupManager.cs
+++ KPSimpleBackup/BasicBackupManager.cs
@@ -1,11 +1,6 @@
 ﻿using KeePassLib;
 using Microsoft.VisualBasic.FileIO;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KPSimpleBackup
 {
@@ -35,31 +30,8 @@ namespace KPSimpleBackup
         protected override void Cleanup()
         {
             string cleanupSearchPattern = dbFileName + "_*" + dbFileExtension;
-            this.CleanupBasic(basePath, cleanupSearchPattern, database.IOConnectionInfo.Path);
-        }
-
-        private void CleanupBasic(String path, String searchPattern, String originalDatabasePath)
-        {
-            int filesToKeepAmount = (int) config.FileAmountToKeep;
-            // read from settings whether to use recycle bin or delete files permanently
-            var recycleOption = config.UseRecycleBinDeletedBackups ? RecycleOption.SendToRecycleBin : RecycleOption.DeletePermanently;
-
-            String[] fileList = Directory.GetFiles(path, searchPattern).OrderBy(f => f).Reverse().ToArray();
-
-            // if more backup files available than needed loop through the unnecessary ones and remove them
-            if (fileList.Count() > filesToKeepAmount)
-            {
-                for (int i = filesToKeepAmount; i < fileList.Count(); i++)
-                {
-                    // never delete original file -> always skip it (in case it made it in the filelist)
-                    if (fileList[i].Equals(originalDatabasePath))
-                    {
-                        continue;
-                    }
-
-                    FileSystem.DeleteFile(fileList[i], UIOption.OnlyErrorDialogs, recycleOption);
-                }
-            }
+            RecycleOption recycleOption = config.UseRecycleBinDeletedBackups ? RecycleOption.SendToRecycleBin : RecycleOption.DeletePermanently;
+            CleanupManager.Cleanup(basePath, cleanupSearchPattern, database.IOConnectionInfo.Path, (int) config.FileAmountToKeep, recycleOption);
         }
     }
 }
---
+++ KPSimpleBackup/CleanupManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.IO;
+using System.Linq;
+
+namespace KPSimpleBackup
+{
+    public static class CleanupManager
+    {
+
+        public static void Cleanup(
+            string path, 
+            string searchPattern,
+            string originalDatabasePath,
+            int amountToKeep,
+            RecycleOption recycleOption
+        ) {
+            string[] fileList = Directory.GetFiles(path, searchPattern).OrderBy(f => f).Reverse().ToArray();
+
+            // if more backup files available than required delete the obsolete files
+            if (fileList.Count() > amountToKeep)
+            {
+                for (int i = amountToKeep; i < fileList.Count(); i++)
+                {
+                    // never delete original file -> always skip it (in case it made it into the filelist)
+                    if (fileList[i].Equals(originalDatabasePath))
+                    {
+                        continue;
+                    }
+
+                    FileSystem.DeleteFile(fileList[i], UIOption.OnlyErrorDialogs, recycleOption);
+                }
+            }
+        }
+
+    }
+}
