558c9e02563d2c0c934925486d094226a0ac41de
marvinweber <marvin@marvinweber.net>
2020-05-15 17:07:39 +0200

    Add abstract BackupManager class
    Extract some functionality from LongTermBackupManager
    to new BackupManager.
    The BackupManager can be used to create further
    managers, like the BasicBackupManager.
    
    see #20

---
+++ KPSimpleBackup/BackupManager.cs
@@ -0,0 +1,151 @@
+﻿
+using KeePass.Resources;
+using KeePassLib;
+using KeePassLib.Interfaces;
+using KeePassLib.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace KPSimpleBackup
+{
+    public abstract class BackupManager
+    {
+
+        protected const string FILE_PREFIX = "file:///";
+        private const string DEFAULT_BACKUP_FILE_EXTENSION = ".kdbx";
+
+        protected virtual string ManagerName { get; set; }
+
+        protected static KPSimpleBackupConfig config;
+
+        protected PwDatabase database;
+
+        protected string dbFileExtension;
+
+        protected string dbFileName;
+
+        protected string basePath;
+
+        protected static Logger pluginLogger = null;
+
+        /// <summary>
+        /// Logger used by the MainWindows of KeePass to show current status
+        /// of saving process.
+        /// </summary>
+        protected static IStatusLogger KPMainWindowSwLogger = null;
+
+        public BackupManager(PwDatabase database)
+        {
+            this.database = database;
+            this.dbFileExtension = GetDbFileExtension();
+            this.dbFileName = GetBackupFileName(database);
+        }
+
+        public static void SetPluginLogger(Logger logger)
+        {
+            pluginLogger = logger;
+        }
+
+        /// <summary>
+        /// Set the logger that should be used for saving databases. This should
+        /// be the logger created by the main KeePass application.
+        /// </summary>
+        /// <param name="logger">Logger to use while saving databases.</param>
+        public static void SetKPMainWindowSwLogger(KeePassLib.Interfaces.IStatusLogger logger)
+        {
+            KPMainWindowSwLogger = logger;
+        }
+
+        /// <summary>
+        /// Set KPSimpleBackupConfig to use.
+        /// </summary>
+        /// <param name="config">Configuration that should be used.</param>
+        public static void SetConfig(KPSimpleBackupConfig config)
+        {
+            BackupManager.config = config;
+        }
+
+        /// <summary>
+        /// Run the actual BackupManager based on the prior
+        /// defined settings / using the prior set database.
+        /// Backups will be created in each backup-directory
+        /// saved in the KPSimpleBackupConfig.
+        /// </summary>
+        public void Run()
+        {
+            pluginLogger.Log("START BackupManager: " + ManagerName + " for database: " + database.Name, LogStatusType.Info);
+
+            List<string> paths = config.BackupPath;
+            foreach (string backupFolderPath in paths)
+            {
+                pluginLogger.Log("Backup to next path:" + backupFolderPath, LogStatusType.Info);
+                basePath = backupFolderPath;
+
+                PreBackup();
+                Backup();
+                Cleanup();
+            }
+
+            pluginLogger.Log("FINISHED BackupManager: " + ManagerName, LogStatusType.Info);
+        }
+
+        protected abstract void PreBackup();
+        protected abstract void Backup();
+        protected abstract void Cleanup();
+
+        protected void SavePwDatabaseToPath(IOConnectionInfo fileInfo)
+        {
+            KPMainWindowSwLogger.StartLogging(KPRes.SavingDatabase, true);
+            database.SaveAs(fileInfo, false, BackupManager.KPMainWindowSwLogger);
+            KPMainWindowSwLogger.EndLogging();
+        }
+
+        protected void SavePwDatabaseToPath(string path)
+        {
+            SavePwDatabaseToPath(IOConnectionInfo.FromPath(path));
+        }
+
+        /// <summary>
+        /// Generate the file extension that should be used.
+        /// If no custom extension is set, the current db-extension
+        /// is used, as a fallback the "standard" file extension
+        /// is used.
+        /// </summary>
+        /// <returns>The database extension to use</returns>
+        private string GetDbFileExtension()
+        {
+            string databaseExtension = DEFAULT_BACKUP_FILE_EXTENSION;
+            try
+            {
+                if (config.UseCustomBackupFileExtension)
+                {
+                    databaseExtension = config.BackupFileExtension;
+                }
+                else
+                {
+                    string databasePath = database.IOConnectionInfo.Path;
+                    databaseExtension = Path.GetExtension(databasePath);
+                }
+            }
+            catch (Exception) { }
+            return databaseExtension;
+        }
+
+        private string GetBackupFileName(PwDatabase database)
+        {
+            // start with database name as 'fallback' / default
+            string backupFileName = database.Name;
+
+            // get file name if database name shouldn't be used
+            if (! config.UseDatabaseNameForBackupFiles)
+            {
+                string path = database.IOConnectionInfo.Path;
+                backupFileName = Path.GetFileNameWithoutExtension(path);
+            }
+
+            return backupFileName;
+        }
+
+    }
+}
